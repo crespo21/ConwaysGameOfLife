5330b567037577f23f542be89a09e7e8ac418e08
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-06-11 15:22:14 +0200

    Update GAmeOfLifeTest.java
    updated GameOfLifeTest class

--- src/test/java/GAmeOfLifeTest.java
+++ src/test/java/GAmeOfLifeTest.java
@@ -1,30 +1,21 @@
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+public class GameOfLifeTest {
 
-public class GAmeOfLifeTest {
     @Test
-    public void AnyLiveCellWithFewerThanTwoLiveNeighborsDies() {
-        int columns=10;
-        int rows =10;
-        int neighbors = 0;
-        int[][] currentState = new int[columns][rows];
-
-        for (int i = -1; i <= 1; i++)
-            for (int j = -1; j <= 1; j++)
-                neighbors +=(currentState[i+i][j+j]);
-        int[][] nextState = new int[columns][rows];
-        for (int i = -1; i <= 1; i++){
-            for (int j = -1; j <= 1; j++) {
-                if ((currentState[i][j] == 1) && (neighbors < 2)) nextState[i][j] = 0;
-                else if ((currentState[i][j] == 1) && (neighbors < 2)) nextState[i][j] = 0;
-                else if ((currentState[i][j] == 1) && (neighbors == 2 || neighbors == 3)) nextState[i][j] = 1;
-                else if ((currentState[i][j] == 1) && (neighbors > 3)) nextState[i][j] = 0;
-                else if ((currentState[i][j] == 0) && (neighbors == 3)) nextState[i][j] = 1;
-                else nextState[i][j] = currentState[i][j];
-            }
-        }
+    public void AnyLiveCellLessTwoNeighbors() {
+        int[][] grid = {
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,1,0,0,0},
+                {0,0,0,0,0,0,1,1,1,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0}
+        };
+        GameOfLife.isState(grid,10,10);
     }
 }
-
